8f9cec3d1b96828bec44da01d11a569bb77b1d85
Tehman Sardar <tehmansardar@gmail.com>
2020-12-13 03:24:54 +0500

    Store Created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import {useState} from 'react'
-import Home from './Home'
-import Profile from './Profile'
 import './App.css';
 
 function App() {
-  const [state, setstate] = useState({name:'Tehman'})
 
-  const changeName = names=>{
-    setstate({name:names})
-  }
   return (
     <div className="App">
-      <Profile name={state} />
-      <Home changeName={changeName} name={state} />
     </div>
   );
 }
