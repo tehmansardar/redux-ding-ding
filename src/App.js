598168ed21f6b7ff931fb6b81a5efbcaea699065
Tehman Sardar <tehmansardar@gmail.com>
2020-12-13 03:49:09 +0500

    Update Redux Store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import {useState} from 'react'
 import {connect} from 'react-redux'
 import './App.css';
 
 function App(props) {
-  console.log(props.myname);
   return (
     <div className="App">
       <h1>My name is {props.myname}</h1>
+      <button
+      onClick={()=>props.changeName('Temi')}
+      >Change</button>
     </div>
   );
 }
@@ -15,5 +16,13 @@ const mapStateToProps = state=>{
     myname:state.name
   }
 }
+const mapDispatchToProps = dispatch=>{
+  return{
+    changeName:name=>{dispatch({
+      type : 'CHANGE_NAME',
+      payload : name
+    })}
+  }
+}
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -4,6 +4,12 @@ const iState = {
 }
 
 const reducer = (state = iState, action) =>{
+    if(action.type ==='CHANGE_NAME'){
+        return{
+            ...state,
+            name : action.payload
+        }
+    }
     return state;
 }
 export default reducer
