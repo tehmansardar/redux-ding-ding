0c957790d8726d4fa5679436ebe1f09b37adc42a
Tehman Sardar <tehmansardar@gmail.com>
2020-12-13 03:34:52 +0500

    Fetch Data into Component from Store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import {useState} from 'react'
+import {connect} from 'react-redux'
 import './App.css';
 
-function App() {
-
+function App(props) {
+  console.log(props.myname);
   return (
     <div className="App">
+      <h1>My name is {props.myname}</h1>
     </div>
   );
 }
+const mapStateToProps = state=>{
+  return{
+    myname:state.name
+  }
+}
 
-export default App;
+export default connect(mapStateToProps)(App);
